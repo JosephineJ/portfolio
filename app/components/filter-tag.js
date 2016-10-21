506c7354d398da9d142e720ec6a6497d9d87c131
jessica-jordan <jessica.jordan.fe.dev@gmail.com>
2016-10-21 21:47:09 +0100

    filling projects with content

--- app/components/filter-tag.js
+++ app/components/filter-tag.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'div',
+  tagName: 'span',
   classNames: ['filter-tag'],
   classNameBindings: ['isActive:active'],
   currentlyActiveFilter: null,
