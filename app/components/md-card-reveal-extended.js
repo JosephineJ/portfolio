be1a1cd41b2764d27c847235516b874c27d23056
Jessica Jordan <jessica.jordan.fe.dev@gmail.com>
2017-09-17 12:27:35 +0200

    gh-pages created

--- app/components/md-card-reveal-extended.js
+++
@@ -1,5 +0,0 @@
-import MdReveal from 'ember-cli-materialize/components/md-card-reveal';
-
-export default MdReveal.extend({
-  classNames: ['md-card-reveal-extended']
-});
--- app/controllers/application.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-  notIndexRoute: Ember.computed('currentRouteName', function(){
-    let currentRoute =  this.get('currentRouteName');
-    return currentRoute !== 'index';
-  })
-});
